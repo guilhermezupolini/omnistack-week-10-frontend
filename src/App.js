dca360a79b239a8f4b11ca4986b905a2eb490a0e
s2it_gzupolini <s2it_gzupolini@uolinc.com>
2020-03-28 00:06:39 -0300

    Finalizacao do codigo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+
+import './global.css';
+import './App.css';
+import './SideBar.css';
+import './Main.css';
+
+import DevItem from './components/DevItem/index';
+import DevForm from './components/DevForm/index';
 
 // voltar o video no time 42:00
 
@@ -7,9 +16,42 @@ import React from 'react';
 // Estado: Informações mantidas pelo componente
 
 function App() {
+  const [devs, setDevs] = useState([])
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+    
+    setDevs([...devs, response.data]);
+
+    console.log(response.data);
+    
+  }
 
   return (
-   <h1>Hello World!</h1>
+   <div id="app">
+    <aside>
+      <strong>Cadastrar</strong>
+      <DevForm onSubmit={handleAddDev} />
+    </aside>
+    <main>
+      <ul>
+        {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+        ))}
+        
+      </ul>
+    </main>
+   </div>
   );
 }
 
